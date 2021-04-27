676d9ba096f85eb47c8defdc857c0ed631f52521
juesuarezca <50672142+juesuarezca@users.noreply.github.com>
2021-04-27 15:57:43 +0200

    Update PINN_cluster.py

--- PINN_cluster.py
+++ PINN_cluster.py
@@ -317,9 +317,7 @@ if __name__ == "__main__":
         return [dirichlet_bc, pde_loss, initial_condition], [bc_dataset, pde_dataset, ic_dataset], Datasets[1]
     
 # Call the datasets functions, losses and weights for the training and for the performance measure
-folder = ''
-cwd = os.getcwd()
-print(cwd)
+folder = 'Results_Simulation'
 [dirichlet_bc_2, pde_loss_2, initial_condition_2], [bc_dataset_2, pde_dataset_2, ic_dataset_2], [boundary_weights_2,
                                                                                      residual_weights_2,
                                                                                      initial_weights_2] = Dataset_loss('Quad', [lb, ub], 1, deg=DEG)
