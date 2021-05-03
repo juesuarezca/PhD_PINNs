a30988a30b6b4aba6d83076b2cf71acb480eccfc
juesuarezca <50672142+juesuarezca@users.noreply.github.com>
2021-05-03 09:49:17 +0200

    Update PINN_cluster.py

--- PINN_cluster.py
+++ PINN_cluster.py
@@ -17,7 +17,7 @@ import sympy as sp
 from mpl_toolkits.mplot3d import Axes3D 
 import sys
 #Read the input variables
-e_l, DEG, n_epoch = [sys.eva[1], sys.eva[2],  sys.eva[3]]
+e_l, DEG, n_epoch = [sys.argv[1], sys.argv[2],  sys.argv[3]]
 #Fixed Data
 DIM = 2
 LP=2
